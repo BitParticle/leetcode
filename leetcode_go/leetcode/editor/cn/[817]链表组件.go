baebc0f16e70447bf45af60f06f33f29e5bb270a
BitParticle <code.zx.peng@gmail.com>
2024-02-11 22:49:09 +0800

    go 链表组件

---
+++ leetcode_go/leetcode/editor/cn/[817]链表组件.go
@@ -0,0 +1,13 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func numComponents(head *ListNode, nums []int) int {
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
