1bad2138f0e3362c878ce45d3bd0e491ec16416a
BitParticle <code.zx.peng@gmail.com>
2024-02-13 23:41:40 +0800

    go 合并二叉树

---
+++ leetcode_go/leetcode/editor/cn/[617]合并二叉树.go
@@ -0,0 +1,14 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
